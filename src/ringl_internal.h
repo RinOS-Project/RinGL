d35d5a1929f8c0e5d79977ac613abb1f8c21e64d
ysmreg <33682244+ysmreg@users.noreply.github.com>
2026-08-22 11:06:05 +0900

    feat: track texture unit state

--- src/ringl_internal.h
+++ src/ringl_internal.h
@@ -19,6 +19,14 @@ typedef struct RinGLBufferObject {
     uint32_t reserved0;
 } RinGLBufferObject;
 
+typedef struct RinGLTextureObject {
+    uint64_t ringpu_image;
+    uint32_t width;
+    uint32_t height;
+    uint32_t format;
+    uint32_t defined;
+} RinGLTextureObject;
+
 typedef struct RinGLShaderObject {
     char* source;
     uint8_t* rsh1;
@@ -86,10 +94,13 @@ struct RinGLContext {
 
     RinGLObjectSlot objects[RINGL_OBJECT_SLOT_COUNT];
     RinGLBufferObject buffers[RINGL_OBJECT_SLOT_COUNT];
+    RinGLTextureObject textures[RINGL_OBJECT_SLOT_COUNT];
     RinGLShaderObject shaders[RINGL_OBJECT_SLOT_COUNT];
     RinGLProgramObject programs[RINGL_OBJECT_SLOT_COUNT];
     uint32_t array_buffer;
     uint32_t element_array_buffer;
+    uint32_t active_texture_unit;
+    uint32_t bound_texture_2d[RINGL_MAX_TEXTURE_UNITS];
     uint32_t current_program;
     RinGLVertexAttribState vertex_attribs[RINGL_MAX_VERTEX_ATTRIBS];
 };
@@ -148,6 +159,7 @@ int ringl_backend_queue_submit(RinGLContext* context,
                                uint64_t command_list);
 void ringl_backend_destroy_object(RinGLContext* context, uint64_t object);
 void ringl_buffer_objects_destroy_all(RinGLContext* context);
+void ringl_texture_objects_destroy_all(RinGLContext* context);
 void ringl_shader_objects_destroy_all(RinGLContext* context);
 void ringl_program_objects_destroy_all(RinGLContext* context);
 void ringl_pipeline_cache_destroy(RinGLContext* context);

1b681b0d33c390445e648473e4af38539c101c69
ysmreg <33682244+ysmreg@users.noreply.github.com>
2026-08-22 13:41:34 +0900

    feat: add varying metadata to GLSL frontend

--- src/shader/glsl_parser.h
+++ src/shader/glsl_parser.h
@@ -8,6 +8,7 @@
 #define RINGL_GLSL_DIAGNOSTIC_MAX 160u
 #define RINGL_GLSL_MAX_SYMBOLS 64u
 #define RINGL_GLSL_MAX_SAMPLER_UNIFORMS 8u
+#define RINGL_GLSL_MAX_VARYINGS 8u
 #define RINGL_GLSL_NAME_MAX 64u
 
 typedef struct RinGLGlslParseResult {
@@ -16,7 +17,10 @@ typedef struct RinGLGlslParseResult {
     uint32_t statement_count;
     uint32_t attribute_count;
     uint32_t sampler_uniform_count;
+    uint32_t varying_count;
     char sampler_uniform_names[RINGL_GLSL_MAX_SAMPLER_UNIFORMS][RINGL_GLSL_NAME_MAX];
+    char varying_names[RINGL_GLSL_MAX_VARYINGS][RINGL_GLSL_NAME_MAX];
+    uint32_t varying_widths[RINGL_GLSL_MAX_VARYINGS];
     char diagnostic[RINGL_GLSL_DIAGNOSTIC_MAX];
 } RinGLGlslParseResult;
 

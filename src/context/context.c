be4044e3c7c7d803b56216aab3533644d054ec4e
ysmreg <33682244+ysmreg@users.noreply.github.com>
2026-08-22 11:06:44 +0900

    feat: release texture objects with contexts

--- src/context/context.c
+++ src/context/context.c
@@ -108,6 +108,7 @@ void ringl_context_destroy(RinGLContext* context)
     ringl_pipeline_cache_destroy(context);
     ringl_program_objects_destroy_all(context);
     ringl_shader_objects_destroy_all(context);
+    ringl_texture_objects_destroy_all(context);
     ringl_buffer_objects_destroy_all(context);
     context->magic = 0u;
     memset(&context->ringpu_ops, 0, sizeof(context->ringpu_ops));

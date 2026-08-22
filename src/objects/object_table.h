c9ee12c21cea451f55780530877f0739ca45a875
ysmreg <33682244+ysmreg@users.noreply.github.com>
2026-08-22 11:04:59 +0900

    feat: add texture object namespace

--- src/objects/object_table.h
+++ src/objects/object_table.h
@@ -11,6 +11,7 @@ typedef enum RinGLObjectType {
     RINGL_OBJECT_BUFFER = 1,
     RINGL_OBJECT_SHADER = 2,
     RINGL_OBJECT_PROGRAM = 3,
+    RINGL_OBJECT_TEXTURE = 4,
 } RinGLObjectType;
 
 typedef enum RinGLObjectState {
